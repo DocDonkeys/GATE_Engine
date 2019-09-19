3c47f60546122dd8e8afaeb4e97672715b866218
Carles Homs <36209557+ch0m5@users.noreply.github.com>
2019-09-19 14:31:44 +0200

    Added MathGeoLib files to solution
    Moved these and ImGui files to Tools filter.

---
+++ GATE_Engine/ModulePhysics.cpp
@@ -0,0 +1,27 @@
+//#include "ModulePhysics.h"
+//
+//ModulePhysics::ModulePhysics(Application * app, bool start_enabled) : Module(app, start_enabled)
+//{
+//}
+//
+//ModulePhysics::~ModulePhysics()
+//{
+//}
+//
+//bool ModulePhysics::Init()
+//{
+//	
+//	return true;
+//}
+//
+//bool ModulePhysics::Start()
+//{
+//
+//	return true;
+//}
+//
+//update_status ModulePhysics::Update(float dt)
+//{
+//
+//	return UPDATE_CONTINUE;
+//}
