a9ba160312cfe5976c8954abc047d0851c966b06
Carles Homs <36209557+ch0m5@users.noreply.github.com>
2019-09-28 21:46:03 +0200

    Added Utilities to Globals.h file

--- GATE_Engine/Globals.h
+++ GATE_Engine/Globals.h
@@ -7,16 +7,52 @@
 #include <windows.h>
 #include <stdio.h>
 
+//LOG
 #define LOG(format, ...) log(__FILE__, __LINE__, format, __VA_ARGS__);
-
 void log(const char file[], int line, const char* format, ...);
 
-#define CAP(n) ((n <= 0.0f) ? n=0.0f : (n >= 1.0f) ? n=1.0f : n=n)
+////Redefines NULL (Careful!)
+//#ifdef NULL
+//#undef NULL
+//#endif
+//#define NULL  0
+//#define NULLRECT {0,0,0,0}
 
-#define DEGTORAD 0.0174532925199432957f
-#define RADTODEG 57.295779513082320876f
-#define HAVE_M_PI
+// Deletes a buffer
+#define RELEASE( x ) \
+    {                        \
+    if( x != NULL )        \
+	    {                      \
+      delete x;            \
+	  x = NULL;              \
+	    }                      \
+    }
 
+// Deletes an array of buffers
+#define RELEASE_ARRAY( x ) \
+    {                              \
+    if( x != NULL )              \
+	    {                            \
+      delete[] x;                \
+	  x = NULL;                    \
+	    }                            \
+                              \
+    }
+
+//Utilities
+#define IN_RANGE( value, min, max ) ( ((value) >= (min) && (value) <= (max)) ? 1 : 0 )	//Returns if within range
+#define CLAMP(x, upper, lower) (MIN(upper, MAX(x, lower)))	//If X is between lower and upper, returns X. Else, if X < lower, returns lower. Else, if X > upper, returns upper.
+#define MIN( a, b ) ( ((a) < (b)) ? (a) : (b) )
+#define MAX( a, b ) ( ((a) > (b)) ? (a) : (b) )
+#define TO_BOOL( a )  ( (a != 0) ? true : false )
+
+// Standard string size
+#define SHORT_STR	32
+#define MID_STR		255
+#define HUGE_STR	8192
+
+//https://wiki.libsdl.org/CategoryAssertions
+//#define SDL_ASSERT_LEVEL 1
 
 typedef unsigned int uint;
 
@@ -36,4 +72,4 @@ enum update_status
 #define WIN_BORDERLESS false
 #define WIN_FULLSCREEN_DESKTOP false
 #define VSYNC true
-#define TITLE "3D Physics Playground"
+#define TITLE "GATE (Garrigo-Approved Template Engine)"
