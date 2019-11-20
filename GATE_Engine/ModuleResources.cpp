d0140eb6bcff73b614a11c19265a55b266617a6e
DidacRomero <36265669+DidacRomero@users.noreply.github.com>
2019-11-20 01:55:45 +0100

    Saving folder & files hierarchy on start

--- GATE_Engine/ModuleResources.cpp
+++ GATE_Engine/ModuleResources.cpp
@@ -1,4 +1,5 @@
 #include "ModuleResources.h"
+#include "ModuleFileSystem.h"
 #include "Application.h"
 #include "Resource.h"
 
@@ -9,6 +10,43 @@ ModuleResources::ModuleResources(Application* app, const char* name, bool start_
 
 ModuleResources::~ModuleResources()
 {
+}
+
+bool ModuleResources::Init()
+{
+	assets_dir.dir_path = ASSETS_FOLDER;
+	InitPopulateAssetsDir(assets_dir);
+	timer.Start();
+	return false;
+}
+
+update_status ModuleResources::PreUpdate(float dt)
+{
+	float time_passed = timer.ReadSec(); //Time passed after update of data
+
+	if (time_passed >= 1.5f)
+	{
+		//std::vector<std::string> discovered_files, discovered_dirs;
+
+		//App->file_system->DiscoverFiles(assets_dir.dir_path.data(),discovered_files, discovered_dirs);
+
+		//int size_diff = assets_dir.sub_dirs.size() - discovered_dirs.size();
+		//
+		//if (size_diff == 0)	//If no subdirectory was added or removed
+		//{
+		//	//Check if the name changed
+		//}
+		//else if (size_diff != 0)	// If sub directory added or removed
+		//{
+		//	// Substitute known dirs by the ones retrieved
+		//	assets_dir.sub_dirs.clear();
+
+		//}
+
+
+		timer.Start();
+	}
+	return update_status::UPDATE_CONTINUE;
 }
 
 uint32 ModuleResources::Find(const char * file_in_assets) const
@@ -39,4 +77,27 @@ Resource * ModuleResources::Get(uint32 uid)
 Resource * ModuleResources::CreateNewResource(Resource::Type type, uint32 force_uid)
 {
 	return nullptr;
+}
+
+void ModuleResources::InitPopulateAssetsDir(AbstractDir &abs_dir)
+{
+	LOG("Loading Assets folder hierarchy.");
+	std::vector<std::string> discovered_files, discovered_dirs;
+
+	App->file_system->DiscoverFiles(abs_dir.dir_path.data(), discovered_files, discovered_dirs);
+
+	for (int i = 0; i < discovered_files.size(); ++i)
+	{
+		abs_dir.files.push_back(discovered_files[i]);
+	}
+
+	for (int i = 0; i < discovered_dirs.size(); ++i)
+	{
+		AbstractDir new_dir;
+		new_dir.dir_path = abs_dir.dir_path + discovered_dirs[i] + "/";
+		abs_dir.sub_dirs.push_back(new_dir);
+	}
+
+	for (int i = 0; i < abs_dir.sub_dirs.size(); ++i)
+		InitPopulateAssetsDir(abs_dir.sub_dirs[i]);
 }
--- GATE_Engine/ModuleResources.h
+++ GATE_Engine/ModuleResources.h
@@ -4,16 +4,27 @@
 #include "Globals.h"
 #include "Module.h"
 #include "Resource.h"
+#include "Timer.h"
 #include <string>
 #include <map>
 
 class Application;
+
+struct AbstractDir
+{
+	std::string dir_path;
+	std::vector<AbstractDir> sub_dirs;
+	std::vector<std::string> files;
+};
 
 class ModuleResources : public Module
 {
 public:
 	ModuleResources (Application* app, const char* name = "null", bool start_enabled = true);
 	~ModuleResources();
+
+	bool Init();
+	update_status PreUpdate(float dt);
 
 public:
 	uint32 Find(const char* file_in_assets) const;
@@ -23,8 +34,17 @@ public:
 	Resource* Get(uint32 uid);
 	Resource* CreateNewResource(Resource::Type type, uint32 force_uid = 0);
 
-	private: 
+private:
+	void InitPopulateAssetsDir(AbstractDir &abs_dir);
+
+private: 
 	std::map<uint32, Resource*> resources;
+
+	//File monitoring
+	Timer timer;
+	std::vector<std::string> known_files;
+	std::vector<std::string> known_dirs;
+	AbstractDir assets_dir;
 };
 #endif // !__MODULE_RESOURCES_H__
 
